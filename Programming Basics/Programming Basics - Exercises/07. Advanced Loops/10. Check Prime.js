62a9952ae238ffbf8f6b937b16d9529bd86ea23f
phiratio <phiratio161@gmail.com>
2017-03-01 04:23:42 +0200

    123

--- Programming Basics/Programming Basics - Exercises/07. Advanced Loops/10. Check Prime.js
+++ Programming Basics/Programming Basics - Exercises/07. Advanced Loops/10. Check Prime.js
@@ -36,7 +36,8 @@ function solve(arr) {
     let x = Number(arr[0]);
 
     function isPrime(Number) {
-            let m = Math.sqrt(x).toFixed(0);
+            let uberX = Math.ceil(x);
+            let m = Math.sqrt(uberX).toFixed(0);
             for (let i = 2; i <= m; i++) {
                 if (x % i === 0) {
                     console.log("Not prime");
@@ -51,21 +52,50 @@ function solve(arr) {
 };
 ////////////////////////////////////////////////////////
 function solve(arr) {
-    let x = Number(arr[0]);
+    let utilities = (function () {
+        let helperApi = {
+            arraySummer: function (array) {
+                return array.reduce(function (a, b) {
+                    return a + b;
+                }, 0);
 
-    x <= 2 ? console.log("Not prime") : getPrimes(x).length === 0 ? console.log("Prime") : console.log("Not prime");
-
-    function getPrimes(max) {
-        let sieve = [], i, j, primes = [];
-        for (i = 2; i <= max; ++i) {
-            if (!sieve[i]) {
-                // i has not been marked -- it is prime
-                primes.push(i);
-                for (j = i << 1; j <= max; j += i) {
-                    sieve[j] = true;
+            },
+            getMaxOfArray: function (numArray) {
+                return Math.max.apply(null, numArray);
+            },
+            getMinOfArray: function (numArray) {
+                return Math.min.apply(null, numArray);
+            },
+            makeAllArrayElementsNumbers: function (array) {
+                return array.map(function (x) {
+                    return parseFloat(x);
+                });
+            },
+            factoriel: function (num) {
+                if (num === 0) {
+                    return 1;
+                } else {
+                    return num * rFact(num - 1);
                 }
             }
+        };
+        return helperApi;
+    })();
+
+    let x = Number(arr[0]);
+
+    function isPrime(Number) {
+        let notPrime = false;
+        let m = Math.sqrt(x).toFixed(0);
+        for (let i = 2; i <= m; i++) {
+            if (x % i === 0) {
+                notPrime = true;
+                break;
+            }
+            notPrime ? console.log("Not prime") : console.log("Prime");
         }
-        return primes;
     }
-}
+
+    x <= 2 ? console.log("Not prime") : isPrime(x) ;
+
+};
