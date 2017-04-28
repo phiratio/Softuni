f9c6a4cf350dc3d5e0179a1c3adbf5e7195c1e21
phiratio <phiratio161@gmail.com>
2017-04-28 09:54:03 +0300

    via cmd

--- Programming Basics/Programming Basics/Programming Basics - Exercises/01. First Steps in Coding/01. Hello C#.js
+++ Programming Basics/Programming Basics/Programming Basics - Exercises/01. First Steps in Coding/01. Hello C#.js
@@ -1,19 +1,6 @@
 /**
  * Created by Void-S on 27-Feb-17.
  */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloCsharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-    {
-        Console.Write("Hello C#!");
-    }
-}
+function solve(arr) {
+    console.log("Hello C#!");
 }
