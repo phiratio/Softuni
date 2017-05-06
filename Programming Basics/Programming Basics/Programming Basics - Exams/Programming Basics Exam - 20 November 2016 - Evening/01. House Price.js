8224d8f64210a75dc1d97f9257e047e32b4ae729
phiratio <phiratio161@gmail.com>
2017-05-06 22:05:05 +0300

    cards problem added

--- Programming Basics/Programming Basics/Programming Basics - Exams/Programming Basics Exam - 20 November 2016 - Evening/01. House Price.js
+++ Programming Basics/Programming Basics/Programming Basics - Exams/Programming Basics Exam - 20 November 2016 - Evening/01. House Price.js
@@ -42,3 +42,68 @@ function solve(arr) {
 }
 
 
+function solve2(arr) {
+    function makeArrayUnique(myArray) {
+        return new Set(myArray);
+    }
+    let textForOutput = "";
+    let joker = arr.indexOf('JOKER');
+    let workingArr = arr.slice(0,joker);
+    let cards = {
+        '1' : 10,
+        '2' : 2,
+        '3' : 3,
+        '4' : 4,
+        '5' : 5,
+        '6' : 6,
+        '7' : 7,
+        '8' : 8,
+        '9' : 9,
+        '10' : 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14,
+        power:{
+            'S' : 4,
+            'H' : 3,
+            'D' : 2,
+            'C' : 1
+        }
+    };
+    let players = {};
+    workingArr.forEach(player => {
+        let name = player.slice(0, player.indexOf(':'));
+        let scores = player.slice(player.indexOf(' ') + 1);
+        if(!players[name]){
+            players[name] = "";
+players[name] += ", " + scores;
+} else {
+    players[name] += ", " + scores;
+}
+});
+for( let prop in players) {
+    let notUniqueCards = players[prop].split(", ").slice(1);
+    let uniqueCards = makeArrayUnique(notUniqueCards);
+    let scores = 0;
+    uniqueCards.forEach(singleCard => {
+        let cardPower = singleCard.slice(-1);
+        let cardValue = singleCard.slice(0, -1);
+        let totalCardPower = cards[cardValue] * cards.power[cardPower];
+        scores += totalCardPower;
+    });
+    if (!prop.contains(":"))
+        textForOutput += `${prop}: ${scores}\n`;
+}
+console.log(textForOutput);
+}
+
+solve2([ 'Pesho: 2C, 4H, 9H, AS, QS',
+    'Slav: 3H, 10S, JC, KD, 5S, 10S',
+    'Peshoslav: QH, QC, QS, QD',
+    'Slav: 6H, 7S, KC, KD, 5S, 10C',
+    'Peshoslav: QH, QC, JS, JD, JC',
+    'Pesho: JD, JD, JD, JD, JD, JD',
+    'JOKER',
+    '',
+    '' ]);
